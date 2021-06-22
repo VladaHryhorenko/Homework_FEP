f45450169b69e8d69210a12159e06537d8fd19e2
VladaHryhorenko <mrsgriggy@gmail.com>
2021-06-22 03:34:55 +0300

    Second task

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,16 @@ console.log("x = ", x, "\ny = ", y, "\nz = ", z, "\n\n\n");
 
 //Task2
 
+//Task1
 
+const x = 50;
+const y = 3 * x;
+const z = x + y;
+console.log("x = ", x, "\ny = ", y, "\nz = ", z, "\n\n\n");
+
+//Task2
+
+let firstName = prompt("Your first name?", "John");
+let lastName = prompt("Your last name?", "Miller");
+let res = "What`s up " + firstName+ " " + lastName;
+alert(res);
