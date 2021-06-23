d7a75f968fe04ce806bf21a3c8f9561b78bcef5d
VladaHryhorenko <mrsgriggy@gmail.com>
2021-06-23 21:17:07 +0300

    Added tasks 4-6

--- js/scripts.js
+++ js/scripts.js
@@ -25,12 +25,48 @@
 
 //Task 3
 
-let age = +prompt("How old are you?", 25);
-if ( age >= 20 && age < 27) {
-    alert ("Выслать повестку :(");
-} else {
-    alert ("Тебе повезло, ты либо ещё слишком молод, либо уже достаточно старый");
-}
+// let age = +prompt("How old are you?", 25);
+// if ( age >= 20 && age < 27) {
+//     alert ("Выслать повестку :(");
+// } else {
+//     alert ("Тебе повезло, ты либо ещё слишком молод, либо уже достаточно старый");
+// }
 
+//Task 4
 
+// for(;;) {
+//     let busNumber = +prompt("Какой приехал автобус(номер)?", 7);
+//     if( busNumber === 7 || busNumber === 225 || busNumber === 255) {
+//         alert ("Едем домой!");
+//         break;
+//     } else {
+//         alert ("Придется ещё подождать своей маршрутки");
+//     }
+// }
+
+//Task 5
+
+// let dayOfTheWeek = "friday"; // = "sunday";
+
+// if (dayOfTheWeek !== "saturday" && dayOfTheWeek !== "sunday") {
+//     alert( `Today is ${dayOfTheWeek}! You have to go to work!` );
+// } else {
+//     alert( `Today is ${dayOfTheWeek}! Have a rest!` );
+// }
+
+//Task 6
+
+// let x = 22;
+// let y = 22;
+// while( (x <= -20) || (x >= 20) ) {
+//     x = +prompt("Enter a number 1( -20 <= number <= 20 )", 0);
+// }
+// while( (y <= -20) || (y >= 20) ) {
+//     y = +prompt("Enter a number 2( -20 <= number <= 20 )", 0);
+// }
+// if ( x <= 1 && (y>=3 || y<0) ) {
+//     alert(`Summ x + y = ${x+y}`);
+// } else {
+//     alert("Неверно! Условие не выполняется!");
+// }
 
