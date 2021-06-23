29b79be6b1800e8c4702a3802a2ba38d0ca80c6b
VladaHryhorenko <mrsgriggy@gmail.com>
2021-06-23 20:44:01 +0300

    Tasks 1-3

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,36 @@
+//Task1
+
+// let x = +prompt("Enter a number", -67);
+// if (x > 0) {
+//     alert ("Number is positive!");
+// } else if (x < 0) {
+//     alert ("Number is negative");
+// } else {
+//     alert ("Number is 0");
+// }
+
+
+//Task2
+
+// let myMoney = +prompt("How much money do YOU have?", 150);
+// let friendsMoney = +prompt("How much money does YOUR FRIEND have?", 100);
+// let allMoney = myMoney + friendsMoney;
+
+// if (allMoney < 1000) {
+//     alert ("It`s not enough for Mallorca! Let`s go to drink some beer!");
+// } else {
+//     alert("We`ll have a nice vacation in Mallorca");
+// }
+
+
+//Task 3
+
+let age = +prompt("How old are you?", 25);
+if ( age >= 20 && age < 27) {
+    alert ("Выслать повестку :(");
+} else {
+    alert ("Тебе повезло, ты либо ещё слишком молод, либо уже достаточно старый");
+}
+
 
 
