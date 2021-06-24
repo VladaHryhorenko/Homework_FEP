c56329b40b0886b47ce4400a27d01503da3e7497
VladaHryhorenko <mrsgriggy@gmail.com>
2021-06-24 09:50:50 +0300

    All tasks - Cycles

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//OPERATORS
+
 //Task1
 
 // let x = +prompt("Enter a number", -67);
@@ -32,6 +34,7 @@
 //     alert ("Тебе повезло, ты либо ещё слишком молод, либо уже достаточно старый");
 // }
 
+
 //Task 4
 
 // for(;;) {
@@ -44,6 +47,7 @@
 //     }
 // }
 
+
 //Task 5
 
 // let dayOfTheWeek = "friday"; // = "sunday";
@@ -53,6 +57,7 @@
 // } else {
 //     alert( `Today is ${dayOfTheWeek}! Have a rest!` );
 // }
+
 
 //Task 6
 
@@ -70,6 +75,7 @@
 //     alert("Неверно! Условие не выполняется!");
 // }
 
+
 //Task 7
 
 // let x = +prompt("Enter namber 1", 7);
@@ -81,6 +87,7 @@
 // } else {
 //     alert("Введенные числа не удовлеворяют условиям");
 // }
+
 
 //Task 8
 
@@ -127,6 +134,7 @@
 //         break;
 // }
 
+
 //Task 10
 
 // let mounth = new Date();
@@ -141,6 +149,7 @@
 //     alert("it`s winter");
 // }
 // console.log(mounth, mounth.getMonth() );
+
 
 //Task 11
 
@@ -218,4 +227,152 @@
 // }
 
 // //(userName === "John" && userAge === 29) || (userName === "Kate" && userAge === 15) ? console.log(`Привет ${userName} ${userAge}`) : console.log("Пока");
+
+
+
+//CYCLES
+
+//Task 1
+
+// let res = "";
+// for (let i = 0; i < 11; i++){
+//     if (i%2 === 0) {
+//         res += ".";
+//     } else {
+//         res += "#";
+//     }
+// }
+// console.log(res);
+
+
+//Task 2
+
+// for (let i = 0; i < 100; i++) {
+//     if (i%2 === 0) {
+//         console.log (Math.pow(i,2));
+//     }
+// }
+
+
+//Task 3
+
+// let username = prompt("What is your name?", "Vlada")
+// for (let j = 0; j < 3; j++) {
+//     console.log("Happy birthday to you");
+//     if (j === 1) {
+//         console.log(`Happy birthday dear ${username}`);
+//     }
+// }
+
+
+//Task 4
+
+// while(true) {
+//     let val = confirm("Нажми Отмена, чтоб продолжить цикл, ОК, чтоб остановить");
+//     if ( val === true) {
+//         break;
+//     }
+// }
+
+//Task 5
+
+// let num = +prompt("Enter a number", 50);
+// let summ = 0;
+// for (let i = 0; i <= num; i++) {
+//     if (i%2 === 1) {
+//         summ += i; 
+//     }
+// }
+// alert(`Summ is ${summ}`);
+
+
+//Task 6
+
+// let count = 0;
+// let randNum = 0;
+// for (;;) {
+//     randNum = Math.random();
+//     count++;
+//     if ( randNum > 0.9 ) {
+//         alert(`Number that broke the cycle: ${randNum}.\nAmount of operations: ${count}`);
+//         break;
+//     }
+// }
+
+
+//Task 7
+
+// for (let i = 1; i < 10; i++){
+//     console.log(`Таблица умножения на ${i}`)
+//     for (let j = 1; j < 10; j++) {
+//             console.log(`${i} * ${j} = ${i * j}`) ;
+//     }
+// }
+
+//Task 8
+
+// let arrA = [0, 1];
+// for (let i = 2; i < 20; arrA[i] = arrA[i-1] + arrA[i-2], i++);
+// console.log(arrA);
+
+
+
+//Task 9
+
+// let x;
+// let count = 0;
+// let summ = 0;
+// while (true) {
+//     x = prompt("Enter a number");
+//     if (x === null) {
+//         break;
+//     }
+//     x = +x;
+//     if ( Number.isNaN(x) ) {
+//         break;
+//     }
+//     count++;
+//     summ += x;
+// }
+// if ( !Number.isNaN(x) ) {
+//     console.log(`Количество введенных чисел: ${count}\nОбщая сумма: ${summ}\nСреднее арифметическое: ${summ / count}`);
+// }
+
+//Task 10
+
+// let password = "123password";
+// let enteredPassword = prompt("Enter a password (length 11max)");
+// let answer;
+
+// while ( !(password === enteredPassword) ) {
+//     if (enteredPassword === null) {
+//         answer = prompt("Are you sure that you want to cancel authorization? (yes/no)");
+//         if ( answer === "yes" ) {
+//             alert("You canceled the authorisation");
+//             break;
+//         }
+//         else
+//             enteredPassword = 0;
+//     }
+//     enteredPassword = prompt("Enter a password (length 11max)");
+// }
+// alert("You are successfully logged in");
+
+//Task 11
+
+for (let k = 1; k <= 50; k++) {
+    if ( k % 3 === 0 &&  k % 5 === 0) {
+        console.log( "FizzBuzz" );
+    } else if ( k % 3 === 0) {
+        console.log( "Fizz" );
+    } else if ( k % 5 === 0) {
+        console.log ( "Buzz")
+    } else {
+        console.log(k);
+    }
+}
+
+
+
+
 
