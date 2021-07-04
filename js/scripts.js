ffa674b025b7d3d8321ca589e2f1ecd8af92a4a0
VladaHryhorenko <mrsgriggy@gmail.com>
2021-07-04 16:04:50 +0300

    Practice2

--- js/scripts.js
+++ js/scripts.js
@@ -1,161 +1,98 @@
-let arr = [
-    {
-        id: 1,
-        name: "John",
-        gender: "m",
-        age: 20
-    },
-    {
-        id: 2,
-        name: "Kate",
-        gender: "f",
-        age: 26    },
-    {
-        id: 3,
-        name: "Lola",
-        gender: "f",
-        age: 28
-    },
-    {
-        id: 4,
-        name: "Harry",
-        gender: "m",
-        age: 66
-    },
-    {
-        id: 5,
-        name: "Nick",
-        gender: "m",
-        age: 45
-    },
-    {
-        id: 6,
-        name: "Bob",
-        gender: "m",
-        age: 12
-    },
-    {
-        id: 7,
-        name: "Ann",
-        gender: "f",
-        age: 15
-    },
-    {
-        id: 8,
-        name: "Marry",
-        gender: "f",
-        age: 17
+//Task1
+
+// function sequence(start = 0, step = 1) {
+//     return function() {
+//       return start +=step;
+//     }
+// }
+
+// let generatorDefault = sequence();
+// let generator = sequence(10, 3);
+// let generator2 = sequence(7, 1);
+// console.log(generatorDefault());
+// console.log(generator());
+// console.log(generator());
+// console.log(generator2());
+
+
+//Task2
+// function reverseStr(str) {
+//     return str.split("").reverse().join("");
+// };
+
+// let str = "123456";
+// console.log(str);
+// let str2 = reverseStr(str);
+// console.log(str2);
+
+//Task 3
+
+// function frstUp(str) {
+//     let arr = str.split(" ");
+//     let arrUppdate = arr.map((item) => {
+//         return `${item[0].toUpperCase()}${item.slice(1)}`
+//     })
+//     return arrUppdate.join(" ");
+// }
+// let str = "hello this is new string";
+// str = frstUp(str);
+// console.log(str);
+
+
+//Task 4
+
+// function modifyStr(str) {
+//     let arr = str.split("_")
+//     let arrUppdate = arr.map((item, index) => {
+//         if (index === 0) return item;
+//         else{
+//             return `${item[0].toUpperCase()}${item.slice(1)}`
+//         }
+//     })
+//     return arrUppdate.join("");
+// }
+
+// let str = "var_text_hello";
+// str = modifyStr(str);
+// console.log(str);
+
+
+//Task 5 
+
+// let numbArr = [1, 2, 3, 4, 5];
+
+// function print(numbArr) {
+//     console.log(numbArr[0]);
+//     numbArr.shift();
+//     if (numbArr.length) {
+//         return print(numbArr);
+//     } else return;
+// }
+
+// print(numbArr);
+
+
+//Task 6
+
+function summOfNumbers(number) {
+    let arr = number.toString().split("");
+    console.log(arr);
+    let summ = 0;
+    for (let i = 0; i < arr.length; i++) {
+        summ += +arr[i];
     }
-];
+    console.log(summ);
+    if (summ > 9) {
+        summOfNumbers(summ);
+    } else return summ;
+}
 
-//forEach - не возвращает ничего, нужно использовать для переборов массива, когда надо узнать какую-либо информацию.
-let summ = 0;
-arr.forEach( (item) => {
-    console.log(item);
-    summ += item.age;
-});
-console.log(summ);
+let number = 987545999876543454;
 
-
-//map - возвращает модифицированный массив, не может вернуть массив меньше чем был - сократить него
-
-let updatedArr = arr.map( (el, index) => {
-    if (index % 2 === 0) {
-        return {
-            ...el,
-            newKey: true,
-            age: el.age+5,
-        }
-    }
-    return el;
-});
-
-console.log(updatedArr);
-
-let usersList = arr.map( (item) => `${item.name} - ${item.age}` );
-console.log(usersList);
-
-//filter - отфильтровать массив - нужно вернуть true/false - отбирает данные из массива - проходит весь массив
-
-let men = arr.filter( item => item.gender === "m" );
-console.log(men);
-
-//find - нужно вернуть true/false - находит первое совпадение и не проходит весь обьект
+let res = summOfNumbers(number);
+console.log(res);
 
 
-let user = arr.find( (item) => item.id = '5');
-console.log(user); // вернул объект
 
-//some - нужно вернуть true/false - есть ли какой-то элемент в массиве
-
-let hasGirls = arr.some( (item)  => item.gender === 'f' );
-console.log(hasGirls);
-
-//every - все ли элементы удовлетворяют условие?
-
-let allAdults = arr.every( (item) => item.age >= 18 );
-console.log(allAdults);
-
-// reduce - 
-
-let summAge = arr.reduce( (acc, item) => {
-    return acc + item.age;
-}, 0 );
-console.log(summAge);
-
-let users = arr.reduce ( (acc, item) => {
-    return {
-        ...acc,
-        [item.name]: item.age
-    }
-}, {});
-console.log(users);
-
-let menList = arr.reduce( (acc, item) => {
-    if (item.gender === 'm') {
-        return [
-            ...acc,
-            {
-                ...item,
-                newKey: true,
-                age: item.age+5 
-            }
-        ];
-    } 
-    return acc;
-}, []);
-console.log(menList);
-
-let array = [
-    {
-        id: 1,
-        name: "John",
-        gender: "m",
-        age: 20
-    },
-    {
-        id: 2,
-        name: "Kate",
-        gender: "f",
-        age: 26    },
-    {
-        id: 3,
-        name: "Lola",
-        gender: "f",
-        age: 28
-    },
-    {
-        id: 4,
-        name: "Harry",
-        gender: "m",
-        age: 66
-    },
-    {
-        id: 5,
-        name: "Nick",
-        gender: "m",
-        age: 45
-    }
-]
 
 
