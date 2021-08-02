a85df48f964835de77815486aa5cd4c0dd4fa802
VladaHryhorenko <mrsgriggy@gmail.com>
2021-08-02 14:05:44 +0300

    HW7

--- js/scripts.js
+++ js/scripts.js
@@ -1,161 +1,87 @@
-let arr = [
-    {
-        id: 1,
-        name: "John",
-        gender: "m",
-        age: 20
-    },
-    {
-        id: 2,
-        name: "Kate",
-        gender: "f",
-        age: 26    },
-    {
-        id: 3,
-        name: "Lola",
-        gender: "f",
-        age: 28
-    },
-    {
-        id: 4,
-        name: "Harry",
-        gender: "m",
-        age: 66
-    },
-    {
-        id: 5,
-        name: "Nick",
-        gender: "m",
-        age: 45
-    },
-    {
-        id: 6,
-        name: "Bob",
-        gender: "m",
-        age: 12
-    },
-    {
-        id: 7,
-        name: "Ann",
-        gender: "f",
-        age: 15
-    },
-    {
-        id: 8,
-        name: "Marry",
-        gender: "f",
-        age: 17
+class Figure {
+    constructor(x, y, color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
     }
-];
+}
 
-//forEach - не возвращает ничего, нужно использовать для переборов массива, когда надо узнать какую-либо информацию.
-let summ = 0;
-arr.forEach( (item) => {
-    console.log(item);
-    summ += item.age;
-});
-console.log(summ);
+class Line extends Figure{
+    constructor(x, y, x2, y2, color) {
+        super(x, y, color);
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+    draw(canvas) {
+        let ctx = canvas.getContext('2d');
+        ctx.strokeStyle = this.color;
+        ctx.beginPath();       // Начинает новый путь
+        ctx.moveTo(this.x, this.y); // Передвигает перо в точку (30, 50)
+        ctx.lineTo(this.x2, this.y2); 
+        ctx.stroke();          // Отображает путь
+    }
+}
 
+class Circle extends Figure{
+    constructor(x, y, r, color) {
+        super(x, y, color);
+        this.r = r;
+    }
+    draw(canvas) {
+        let ctx = canvas.getContext('2d');
+        ctx.fillStyle = this.color;
+        console.log(this.color);
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+}
 
-//map - возвращает модифицированный массив, не может вернуть массив меньше чем был - сократить него
+class Rect extends Figure{
+    constructor(x, y, w, h, color) {
+        super(x, y, color);
+        this.w = w;
+        this.h = h;
+    }
+    draw(canvas){
+        let ctx = canvas.getContext('2d');
+        ctx.fillStyle = this.color;
+        console.log(this.color);
+        console.log(this.x, this.y, this.w, this.h);
+        ctx.fillRect(this.x, this.y, this.w, this.h);
+    }
+}
 
-let updatedArr = arr.map( (el, index) => {
-    if (index % 2 === 0) {
-        return {
-            ...el,
-            newKey: true,
-            age: el.age+5,
+class Canvas {
+    constructor(id) {
+        this.canvas = document.getElementById(id);
+    }
+    add() {
+        for (let i = 0; i < arguments.length; i++) {
+            arguments[i].draw(this.canvas);
         }
     }
-    return el;
-});
-
-console.log(updatedArr);
-
-let usersList = arr.map( (item) => `${item.name} - ${item.age}` );
-console.log(usersList);
-
-//filter - отфильтровать массив - нужно вернуть true/false - отбирает данные из массива - проходит весь массив
-
-let men = arr.filter( item => item.gender === "m" );
-console.log(men);
-
-//find - нужно вернуть true/false - находит первое совпадение и не проходит весь обьект
+}
 
 
-let user = arr.find( (item) => item.id = '5');
-console.log(user); // вернул объект
-
-//some - нужно вернуть true/false - есть ли какой-то элемент в массиве
-
-let hasGirls = arr.some( (item)  => item.gender === 'f' );
-console.log(hasGirls);
-
-//every - все ли элементы удовлетворяют условие?
-
-let allAdults = arr.every( (item) => item.age >= 18 );
-console.log(allAdults);
-
-// reduce - 
-
-let summAge = arr.reduce( (acc, item) => {
-    return acc + item.age;
-}, 0 );
-console.log(summAge);
-
-let users = arr.reduce ( (acc, item) => {
-    return {
-        ...acc,
-        [item.name]: item.age
-    }
-}, {});
-console.log(users);
-
-let menList = arr.reduce( (acc, item) => {
-    if (item.gender === 'm') {
-        return [
-            ...acc,
-            {
-                ...item,
-                newKey: true,
-                age: item.age+5 
-            }
-        ];
-    } 
-    return acc;
-}, []);
-console.log(menList);
-
-let array = [
-    {
-        id: 1,
-        name: "John",
-        gender: "m",
-        age: 20
-    },
-    {
-        id: 2,
-        name: "Kate",
-        gender: "f",
-        age: 26    },
-    {
-        id: 3,
-        name: "Lola",
-        gender: "f",
-        age: 28
-    },
-    {
-        id: 4,
-        name: "Harry",
-        gender: "m",
-        age: 66
-    },
-    {
-        id: 5,
-        name: "Nick",
-        gender: "m",
-        age: 45
-    }
-]
+let line = new Line(100, 800, 500, 740, 'gray'); // x1, y1, x2, y2, color
+let line2 = new Line(100, 820, 500, 760, 'gray'); // x1, y1, x2, y2, color
+let circle = new Circle(300, 300, 50, 'rgba(8,51,199,0.4)'); // x, y, r, color
+let circle2 = new Circle(340, 380, 100, 'rgba(8,51,199,0.4)'); // x, y, r, color
+let rect = new Rect(900, 350, 150, 250, 'rgba(0,208,80,0.3)'); // x, y, w, h, color
+let rect2 = new Rect(960, 320, 180, 100, 'rgba(255,101,80,0.4)'); // x, y, w, h, color
+let rect3 = new Rect(1100, 390, 100, 70, 'rgba(255,255,0,0.4)'); // x, y, w, h, color
 
 
+let drawArea = new Canvas('canvasID');
+
+let i = 0;
+while (i !== 1500) {
+    drawArea.add(new Line(i, 0, (i+30), 30, 'red'));
+    drawArea.add(new Line((i+30), 30, (i+60), 0, 'red'));
+    i += 60;
+}
+
+drawArea.add(circle, circle2);
+drawArea.add(rect, rect2, rect3);
+drawArea.add(line, line2);
