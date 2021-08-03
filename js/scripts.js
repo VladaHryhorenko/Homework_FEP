b449d2d9b0595c1fce50f75abe5dc853347a461c
VladaHryhorenko <mrsgriggy@gmail.com>
2021-08-03 18:27:22 +0300

    HW8 improved

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,36 @@
-let container = document.getElementById('container');
+const container = document.createElement('div');
+document.body.appendChild(container);
+document.body.style.display = 'flex';
+document.body.style.justifyContent = 'center';
+document.body.style.fontFamily = 'Roboto';
+container.style.border = "3px solid rgb(31, 35, 56)"
+container.style.width = "50%";
+
+const buttonBlock = document.createElement('div');
+buttonBlock.classList.add('button-block');
+container.appendChild(buttonBlock);
+buttonBlock.style.display = 'flex';
+buttonBlock.style.justifyContent = 'center';
+
+
+const startButton = document.createElement('button');
+const stopButton = document.createElement('button');
+startButton.innerText = 'Start';
+stopButton.innerText = 'Stop';
+stopButton.style.margin = "20px 25px";
+stopButton.style.padding = "10px 16px";
+stopButton.style.backgroundColor = "rgb(255, 69, 0)";
+stopButton.style.borderRadius = '20px'
+startButton.style.margin = "20px 25px";
+startButton.style.padding = "10px 16px";
+startButton.style.backgroundColor = "rgb(144,238,144)";
+startButton.style.borderRadius = '20px'
+
+buttonBlock.appendChild(startButton);
+buttonBlock.appendChild(stopButton)
+
 let el = document.createElement('h2');
-const start = document.getElementById('start');
-const stoped = document.getElementById('stop');
+el.style.textAlign = 'center';
 let date = new Date();
 let interval;
 
@@ -20,8 +49,8 @@ function stopClock() {
     el.innerText = `Время: ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} `;
 }
 
-start.addEventListener('click', clockFunc);
-stoped.addEventListener('click', stopClock);
+startButton.addEventListener('click', clockFunc);
+stopButton.addEventListener('click', stopClock);
 
 
 
